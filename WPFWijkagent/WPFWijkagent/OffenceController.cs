2a059b833dce7d118193b82ca0c49304d721d8a7
Randor8 <randor.stephen@gmail.com>
2019-11-19 10:10:58 +0100

    added offence controller,offence view and comments

---
+++ WPFWijkagent/WPFWijkagent/OffenceController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WijkagentModels;
+
+namespace WijkagentWPF
+{
+    /// <summary>
+    /// controlls the offence(s) for the wpf application
+    /// </summary>
+    public class OffenceController
+    {
+        private Offence _offence { get; set; }
+
+        public OffenceController()
+        {
+            _offence = new Offence();
+        }
+
+        /// <summary>
+        /// sets the offence location
+        /// </summary>
+        /// <param name="location">the location object to add</param>
+        public void SetOffenceData(Location location)
+        {
+            SetOffenceData("", "", new DateTime().ToLocalTime(), location);
+        }
+
+        /// <summary>
+        /// sets the form fields for the offence 
+        /// </summary>
+        /// <param name="description">offence description</param>
+        /// <param name="category">offence Categorie(enum value)</param>
+        /// <param name="dateTime">offence date and time</param>
+        public void SetOffenceData(string description, string category, DateTime dateTime)
+        {
+            SetOffenceData(description, category, dateTime, _offence.LocationID);
+        }
+
+        /// <summary>
+        /// sets all the offence fields
+        /// </summary>
+        /// <param name="description">offence description</param>
+        /// <param name="category">offence Categorie(enum value)</param>
+        /// <param name="dateTime">offence date and time</param>
+        /// <param name="location">offence location</param>
+        public void SetOffenceData(string description, string category, DateTime dateTime, Location location)
+        {
+            _offence.Category = category;
+            _offence.Description = description;
+            _offence.DateTime = dateTime;
+            //TODO: save the location as a separate object
+            _offence.LocationID = location;
+        }
+
+        /// <summary>
+        /// gets all the offences from the db
+        /// </summary>
+        /// <returns></returns>
+        public List<Offence> GetOffences()
+        {
+            return Offence.OffenceData();
+        }
+    }
+}
--- WPFWijkagent/WijkagentModels/Offence.cs
+++ WPFWijkagent/WijkagentModels/Offence.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WijkagentModels
 {
     public class Offence
     {
+        //returns a list of 4 offence objects
+        public static List<Offence> OffenceData()
+        {
+            return new List<Offence>()
+            {
+                new Offence(){
+                    ID=1,
+                    DateTime= new DateTime().ToLocalTime(),
+                    Description = "een delict..",
+                    LocationID= new Location(){ 
+                        Longitude=52.499853m,
+                        Latitude=6.081799m
+                    },
+                },
+                new Offence(){
+                    ID=2,
+                    DateTime= new DateTime().ToLocalTime(),
+                    Description = "een delict..",
+                    LocationID= new Location(){
+                        Longitude=52.498599m,
+                        Latitude=6.083880m
+                    },
+                },
+                new Offence(){
+                    ID=3,
+                    DateTime= new DateTime().ToLocalTime(),
+                    Description = "een delict..",
+                    LocationID= new Location(){
+                        Longitude=52.497853m,
+                        Latitude=6.080799m
+                    },
+                },
+                new Offence(){
+                    ID=4,
+                    DateTime= new DateTime().ToLocalTime(),
+                    Description = "een delict..",
+                    LocationID= new Location(){
+                        Longitude=52.50853m,
+                        Latitude=6.084099m
+                    },
+                }
+            };
+        }
         public int ID { get; set; }
-        public DateTime Time { get; set; }
+        public DateTime DateTime { get; set; }
         public string Description { get; set; }
+        public String Category { get; set; }
         public virtual Location LocationID { get; set; }
     }
 }
