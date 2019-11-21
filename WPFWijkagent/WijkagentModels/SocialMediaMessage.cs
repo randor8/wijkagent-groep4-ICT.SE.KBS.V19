0184a2d5c04100d11b26ab02fe717423f1190d75
Lord Queek <49435919+LordQueekcodebreaker@users.noreply.github.com>
2019-11-21 13:01:40 +0100

    Revert "Merge pull request #7 from randor8/test"
    This reverts commit dbf55fa8a3812c6912f4d66e20b34cfde2126996.

--- WPFWijkagent/WijkagentModels/SocialMediaMessage.cs
+++ WPFWijkagent/WijkagentModels/SocialMediaMessage.cs
@@ -10,18 +10,5 @@ namespace WijkagentModels
         public DateTime DateTime { get; set; }
         public string Message { get; set; }
         public virtual Location LocationID { get; set; }
-
-        public SocialMediaMessage(int id, DateTime time, string message, Location location)
-        {
-            ID = id;
-            Time = time;
-            Message = message;
-            LocationID = location;
-        }
-
-        public override string ToString()
-        {
-            return $"Id: {ID}\nDate: {Time}\nContent: {Message}\nLocation: {LocationID.Latitude} : {LocationID.Longitude}";
-        }
     }
 }
