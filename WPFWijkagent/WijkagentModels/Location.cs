3d51602e333f5890d303657d09dccb3f8e4140e1
Boukje Maria Steur <57618134+frutsel32@users.noreply.github.com>
2019-11-21 14:08:22 +0100

    Revert "Merge branch 'test' into 2_delicten_overzicht"
    This reverts commit 3d5702cfb30a45e86861a9c4cd01399f3df13f82.

--- WPFWijkagent/WijkagentModels/Location.cs
+++ WPFWijkagent/WijkagentModels/Location.cs
@@ -9,11 +9,5 @@ namespace WijkagentModels
         public int ID { get; set; }
         public double Longitude { get; set; }
         public double Latitude { get; set; }
-
-        public Location(double latitude, double longitude)
-        {
-            Latitude = latitude;
-            Longitude = longitude;
-        }
     }
 }
