7828ccde2041fff32c199a7d30f4d1a559392e1c
VimtoFive <joost.oltmans@gmail.com>
2019-12-17 14:09:29 +0100

    Datum aangepast

---
+++ WPFWijkagent/WPFWijkagent/ContactWitnessDialogue.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WijkagentModels;
+
+namespace WijkagentWPF
+{
+    /// <summary>
+    /// Interaction logic for ContactWitnessDialogue.xaml
+    /// </summary>
+    public partial class ContactWitnessDialogue : Window
+    {
+        public ContactWitnessDialogueController dialogueController = new ContactWitnessDialogueController(); 
+
+        public ContactWitnessDialogue()
+        {
+            InitializeComponent();
+        }
+
+        public void PrintMessage(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
