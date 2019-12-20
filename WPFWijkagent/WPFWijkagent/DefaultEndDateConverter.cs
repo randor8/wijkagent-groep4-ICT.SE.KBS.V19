2ea3a534038ebb59898f25f69f3356177ca3f90f
Unknown <jjtiessen@hotmail.com>
2019-12-20 14:56:23 +0100

    Add some comments to Converter

--- WPFWijkagent/WPFWijkagent/DefaultEndDateConverter.cs
+++ WPFWijkagent/WPFWijkagent/DefaultEndDateConverter.cs
@@ -6,6 +6,14 @@ namespace WijkagentWPF
 {
     public class DefaultEndDateConverter : IValueConverter
     {
+        /// <summary>
+        /// Limits the given date to a date in the past or the current date.
+        /// </summary>
+        /// <param name="value">DateTime object.</param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is DateTime dateTime)
@@ -15,6 +23,14 @@ namespace WijkagentWPF
             return DateTime.Today;
         }
 
+        /// <summary>
+        /// This method should not be used.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return null;
