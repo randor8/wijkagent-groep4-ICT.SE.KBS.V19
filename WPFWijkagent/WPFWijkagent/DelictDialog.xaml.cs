3a412fce7550303ccaa38e02fd1a44d41bbf0ab3
Boukje Maria Steur <boukje@switchware.io>
2020-01-27 11:02:57 +0100

    extra spaces

--- WPFWijkagent/WPFWijkagent/DelictDialog.xaml.cs
+++ WPFWijkagent/WPFWijkagent/DelictDialog.xaml.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -252,4 +249,3 @@ namespace WijkagentWPF
         }
     }
 }
-
